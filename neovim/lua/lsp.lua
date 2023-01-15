628b2eea71347614bfc93fed14536e6428699eb6
Gideon Farrell <gideon@gtf.io>
2023-01-15 15:15:45 +0000

    [neovim] lsp config updates

--- neovim/lua/lsp.lua
+++ neovim/lua/lsp.lua
@@ -1,21 +1,51 @@
--- Enable LSP configurations
-require'lspconfig'.hls.setup{}
+-- Autocomplete
+vim.opt.completeopt = "menu,menuone,noselect"
+local cmp = require'cmp'
+cmp.setup({
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    { name = 'buffer' },
+    { name = 'path' }
+  }),
 
--- LSP Keymaps
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end
+  },
+
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true })
+  })
+})
+
+-- General LSP Keymaps
+local kmopts = { noremap = true, silent = true }
+vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist, opts)
+
+-- Buffer-specific LSP Keymaps
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
-    -- autocomplete on <C-.>
-    vim.keymap.set('i', '<C-.>', vim.lsp.buf.completion)
-    -- format on ,f
-    vim.keymap.set('n', '<Leader>f', vim.lsp.buf.format, { buffer = args.buf })
-    -- go to next and previous diagnostics
-    vim.keymap.set('n', '<Leader>dj', vim.diagnostic.goto_next)
-    vim.keymap.set('n', '<Leader>dk', vim.diagnostic.goto_prev)
-    -- set hover
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
-    -- find all references
-    vim.keymap.set('n', 'R', vim.lsp.buf.references, { buffer = args.buf })
-    -- rename symbol
-    vim.keymap.set('n', '<Leader>r', vim.lsp.buf.rename, { buffer = args.buf })
+    local bufopts = { buffer = args.buf, silent = true, noremap = true }
+    vim.keymap.set('n', '<Leader>f', vim.lsp.buf.format, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set({ 'n', 'x' }, '<Leader>ca', vim.lsp.buf.code_action, bufopts)
   end,
 })
+
+-- Enable LSP configurations
+require'lspconfig'.hls.setup{}
+require'lspconfig'.sqlls.setup{}
+require'lspconfig'.elmls.setup{}
