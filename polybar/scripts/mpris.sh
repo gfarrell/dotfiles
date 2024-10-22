10f41018671c01be0368f9bf213a62d4e2ad16cf
Gideon Farrell <gideon@gtf.io>
2024-10-22 22:13:58 +0100

    fix mpris status when no player connected

--- polybar/scripts/mpris.sh
+++ polybar/scripts/mpris.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
 
 title=$(playerctl metadata --format "{{title}} - {{artist}}" 2>/dev/null)
-case $(playerctl status) in
+# time="$(playerctl position --format "{{duration(position)}}") / $(playerctl metadata --format "{{duration(mpris:length)}}")"
+case $(playerctl status 2>&1) in
   Playing)
     echo "\uf04b ${title}"
     ;;
