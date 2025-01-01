71b26c777a9ca01d564c1ad50c4c75a9ddfe7bf9
Gideon Farrell <gideon@gtf.io>
2025-01-01 18:30:32 +0000

    update neorg config

--- neovim/lua/plugins/neorg.lua
+++ neovim/lua/plugins/neorg.lua
@@ -7,8 +7,6 @@ return {
       require("neorg").setup {
         load = {
           ["core.defaults"] = {},
-          ["core.concealer"] = {},
-          ["core.integrations.nvim-cmp"] = {},
           ["core.dirman"] = {
             config = {
               workspaces = {
@@ -17,9 +15,31 @@ return {
               index = "index.norg",
               default_workspace = "kb"
             }
+          },
+          ["core.concealer"] = {},
+          ["core.completion"] = {
+            config = {
+              engine = "nvim-cmp",
+            }
+          },
+          ["core.integrations.nvim-cmp"] = {},
+          ["core.text-objects"] = {},
+          ["core.qol.todo_items"] = {
+            config = {
+              create_todo_items = true,
+            }
+          },
+          ["core.journal"] = {
+            config = {
+              workspace = "kb"
+            }
           }
         }
       }
+      vim.keymap.set("n", "<up>", "<Plug>(neorg.text-objects.item-up)", {})
+      vim.keymap.set("n", "<down>", "<Plug>(neorg.text-objects.item-down)", {})
+      vim.keymap.set({ "o", "x" }, "iH", "<Plug>(neorg.text-objects.textobject.heading.inner)", {})
+      vim.keymap.set({ "o", "x" }, "aH", "<Plug>(neorg.text-objects.textobject.heading.outer)", {})
     end
   }
 }
