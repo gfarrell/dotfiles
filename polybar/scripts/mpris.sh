b86919c55e9ffc4fbb98d6b3569120e5a01d66c9
Gideon Farrell <gideon@gtf.io>
2025-08-11 11:35:52 +0100

    [polybar] truncate song/artist names to stop them taking over the bar

--- polybar/scripts/mpris.sh
+++ polybar/scripts/mpris.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-title=$(playerctl metadata --format "{{title}} - {{artist}}" 2>/dev/null)
+title=$(playerctl metadata --format "{{trunc(title, 30)}} - {{trunc(artist, 30)}}" 2>/dev/null)
 # time="$(playerctl position --format "{{duration(position)}}") / $(playerctl metadata --format "{{duration(mpris:length)}}")"
 case $(playerctl status 2>&1) in
   Playing)
