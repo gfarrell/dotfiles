c16b5290689304829bfc4fe8fd9e51c067efe79d
Gideon Farrell <gideon@gtf.io>
2025-11-28 18:32:43 +0000

    [nvim] don't use aerial for {}

--- neovim/lua/plugins/editor.lua
+++ neovim/lua/plugins/editor.lua
@@ -67,13 +67,6 @@ return {
     },
     init = function()
       vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
-    end,
-    opts = {
-      on_attach = function(bufnr)
-        -- Jump forwards/backwards with '{' and '}'
-        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-      end
-    }
+    end
   }
 }
