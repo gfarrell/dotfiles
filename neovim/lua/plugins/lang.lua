45b3f0901b1b238bf462f910854c7bdc3f3c328b
Gideon Farrell <gideon@gtf.io>
2026-04-23 16:19:04 +0100

    [nvim] use the latest nvim-treesitter plugin

--- neovim/lua/plugins/lang.lua
+++ neovim/lua/plugins/lang.lua
@@ -2,78 +2,44 @@ return {
   -- Language parsing with Treesitter
   {
     'nvim-treesitter/nvim-treesitter',
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
     config = function()
-      local treesitter = require("nvim-treesitter.configs")
-
-      treesitter.setup({
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-        },
-        ensure_installed = {
-          "bash",
-          "c",
-          "clojure",
-          "cpp",
-          "css",
-          "csv",
-          "desktop",
-          "dhall",
-          "dockerfile",
-          "git_rebase",
-          "gitcommit",
-          "gitignore",
-          "haskell",
-          "html",
-          "json",
-          "lua",
-          "make",
-          "markdown",
-          "markdown_inline",
-          "nix",
-          "norg",
-          "purescript",
-          "query",
-          "rust",
-          "toml",
-          "tsx",
-          "typescript",
-          "typst",
-          "vim",
-          "vimdoc",
-          "xresources",
-          "yaml",
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          }
-        },
-      })
-    end
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    lazy = true, -- only load with the dependent
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-          },
-        },
-      })
+      require("nvim-treesitter").install { 
+        "bash",
+        "c",
+        "clojure",
+        "cpp",
+        "css",
+        "csv",
+        "desktop",
+        "dhall",
+        "dockerfile",
+        "git_rebase",
+        "gitcommit",
+        "gitignore",
+        "haskell",
+        "html",
+        "json",
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "nix",
+        "norg",
+        "purescript",
+        "query",
+        "rust",
+        "toml",
+        "tsx",
+        "typescript",
+        "typst",
+        "vim",
+        "vimdoc",
+        "xresources",
+        "yaml",
+      }
     end
   },
 
